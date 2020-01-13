d1d5804973b7b1e0b0542b5a08f23cb36c750707
joviksdev <toch.emeo@gmail.com>
2020-01-14 00:56:54 +0100

    Updates

---
+++ precache-manifest.f9092887422d6fe2f21c090778a9ead5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36179fa24c8a9b925d4f31bac1e072b4",
+    "url": "/React-Todo-Application/index.html"
+  },
+  {
+    "revision": "47508b98da9dba17bc69",
+    "url": "/React-Todo-Application/static/css/main.de71a3e8.chunk.css"
+  },
+  {
+    "revision": "aefa6035cd95b75754bf",
+    "url": "/React-Todo-Application/static/js/2.35a4c399.chunk.js"
+  },
+  {
+    "revision": "d0cb0a244b3563727e54b0f5d5982212",
+    "url": "/React-Todo-Application/static/js/2.35a4c399.chunk.js.LICENSE"
+  },
+  {
+    "revision": "47508b98da9dba17bc69",
+    "url": "/React-Todo-Application/static/js/main.1f485725.chunk.js"
+  },
+  {
+    "revision": "9d2279024ae46bf3d108",
+    "url": "/React-Todo-Application/static/js/runtime-main.8b088879.js"
+  }
+]);
