d7aca8023c46b217db9e696d027cee80820e4b37
joviksdev <toch.emeo@gmail.com>
2020-01-14 01:07:51 +0100

    modify TodoItem.js file, remove spinner.gif

--- src/component/todo/TodoItem.js
+++ src/component/todo/TodoItem.js
@@ -15,36 +15,32 @@ const TodoItem = () => {
     delTodo(e.target.id);
   };
 
-  if (loading) {
-    return <Spinner />;
-  } else {
-    return todos.map(todo => {
-      const id = todos.indexOf(todo);
-      const setStyle = () => ({
-        padding: '0.4rem 0',
-        color: todo.completed ? 'gray' : '#141110',
-        textDecoration: todo.completed ? 'line-through' : '',
-        fontStyle: todo.completed ? 'italic' : ''
-      });
-
-      return (
-        <div className='container todo-item' key={id}>
-          <div>
-            <input type='checkbox' id={id} onChange={onChange} />{' '}
-            <label>Completed</label>
-          </div>
-          <p style={setStyle()}>{todo.activity}</p>
-          <input
-            className='btn btn-delete'
-            onClick={setDelete}
-            id={id}
-            type='button'
-            value='Delete'
-          />
-        </div>
-      );
+  return todos.map(todo => {
+    const id = todos.indexOf(todo);
+    const setStyle = () => ({
+      padding: '0.4rem 0',
+      color: todo.completed ? 'gray' : '#141110',
+      textDecoration: todo.completed ? 'line-through' : '',
+      fontStyle: todo.completed ? 'italic' : ''
     });
-  }
+
+    return (
+      <div className='container todo-item' key={id}>
+        <div>
+          <input type='checkbox' id={id} onChange={onChange} />{' '}
+          <label>Completed</label>
+        </div>
+        <p style={setStyle()}>{todo.activity}</p>
+        <input
+          className='btn btn-delete'
+          onClick={setDelete}
+          id={id}
+          type='button'
+          value='Delete'
+        />
+      </div>
+    );
+  });
 };
 
 export default TodoItem;
